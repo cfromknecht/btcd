33f411e709aee42e3a05fa84f68c32107c74c6d7
Conner Fromknecht <conner@lightning.engineering>
2019-04-19 19:07:18 -0700

    txscript: rename calcSignatureHashRaw

--- txscript/opcode.go
+++ txscript/opcode.go
@@ -1992,7 +1992,7 @@ func opcodeCheckSig(op *parsedOpcode, vm *Engine) error {
 		// to sign itself.
 		subScript = removeOpcodeByData(subScript, fullSigBytes)
 
-		hash = calcSignatureHashRaw(subScript, hashType, &vm.tx, vm.txIdx)
+		hash = calcSignatureHash(subScript, hashType, &vm.tx, vm.txIdx)
 	}
 
 	pubKey, err := btcec.ParsePubKey(pkBytes, btcec.S256())
@@ -2261,7 +2261,7 @@ func opcodeCheckMultiSig(op *parsedOpcode, vm *Engine) error {
 				return err
 			}
 		} else {
-			hash = calcSignatureHashRaw(script, hashType, &vm.tx, vm.txIdx)
+			hash = calcSignatureHash(script, hashType, &vm.tx, vm.txIdx)
 		}
 
 		var valid bool
