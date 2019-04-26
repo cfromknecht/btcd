decred/dcrd

txscript: Optimize ExtractPkScriptAddrs nulldata.
This completes the process of converting the ExtractPkScriptAddrs
function to use the optimized extraction functions recently introduced
as part of the typeOfScript conversion.

In particular, this converts the detection for nulldata scripts, removes
the slow path fallback code since it is the final case, and modifies the
comment to call out the script version semantics.

The following is a before and after comparison of analyzing both a
typical standard script and a very large non-standard script:

benchmark                            old ns/op    new ns/op    delta
-----------------------------------------------------------------------
BenchmarkExtractPkScriptAddrsLarge   132400       44.4         -99.97%
BenchmarkExtractPkScriptAddrs        1265         231          -81.74%

benchmark                            old allocs   new allocs   delta
-----------------------------------------------------------------------
BenchmarkExtractPkScriptAddrsLarge   1            0            -100.00%
BenchmarkExtractPkScriptAddrs        5            2            -60.00%

benchmark                            old bytes    new bytes    delta
-----------------------------------------------------------------------
BenchmarkExtractPkScriptAddrsLarge   466944       0            -100.00%
BenchmarkExtractPkScriptAddrs        1600         48           -97.00%

txscript/standard.go
@@ -1414,14 +1414,15 @@ func scriptHashToAddrs(hash []byte, params *chaincfg.Params) []dcrutil.Address {
 // signatures associated with the passed PkScript.  Note that it only works for
 // 'standard' transaction script types.  Any data such as public keys which are
 // invalid are omitted from the results.
+//
+// NOTE: This function only attempts to identify version 0 scripts.  The return
+// value will indicate a nonstandard script type for other script versions along
+// with an invalid script version error.
 func ExtractPkScriptAddrs(version uint16, pkScript []byte,
 	chainParams *chaincfg.Params) (ScriptClass, []dcrutil.Address, int, error) {
 	if version != 0 {
 		return NonStandardTy, nil, 0, fmt.Errorf("invalid script version")
 	}
-
-	// Avoid parsing the script for the cases that already have the able to
-	// work with raw scripts.
 
 	// Check for pay-to-pubkey-hash script.
 	if hash := extractPubKeyHash(pkScript); hash != nil {
@@ -1530,25 +1531,15 @@ func ExtractPkScriptAddrs(version uint16, pkScript []byte,
 		return StakeSubChangeTy, scriptHashToAddrs(hash, chainParams), 1, nil
 	}
 
-	// Fall back to slow path.  Ultimately these are intended to be replaced by
-	// faster variants based on the unparsed raw scripts.
-
-	var addrs []dcrutil.Address
-	var requiredSigs int
-
-	scriptClass := typeOfScript(version, pkScript)
-
-	switch scriptClass {
-	case NullDataTy:
-		// Null data transactions have no addresses or required
-		// signatures.
-
-	case NonStandardTy:
-		// Don't attempt to extract addresses or required signatures for
-		// nonstandard transactions.
+	// Check for null data script.
+	if isNullDataScript(version, pkScript) {
+		// Null data transactions have no addresses or required signatures.
+		return NullDataTy, nil, 0, nil
 	}
 
-	return scriptClass, addrs, requiredSigs, nil
+	// Don't attempt to extract addresses or required signatures for nonstandard
+	// transactions.
+	return NonStandardTy, nil, 0, nil
 }
 
 // extractOneBytePush returns the value of a one byte push.

cfromknecht/btcd

txscript/standard.go
@@ -749,10 +749,11 @@ func scriptHashToAddrs(hash []byte, params *chaincfg.Params) []btcutil.Address {
 // signatures associated with the passed PkScript.  Note that it only works for
 // 'standard' transaction script types.  Any data such as public keys which are
 // invalid are omitted from the results.
+//
+// NOTE: This function only attempts to identify version 0 scripts.  The return
+// value will indicate a nonstandard script type for other script versions along
+// with an invalid script version error.
 func ExtractPkScriptAddrs(pkScript []byte, chainParams *chaincfg.Params) (ScriptClass, []btcutil.Address, int, error) {
-
-	// Avoid parsing the script for the cases that already have the able to
-	// work with raw scripts.
 
 	// Check for pay-to-pubkey-hash script.
 	if hash := extractPubKeyHash(pkScript); hash != nil {
@@ -789,6 +790,12 @@ func ExtractPkScriptAddrs(pkScript []byte, chainParams *chaincfg.Params) (Script
 		return MultiSigTy, addrs, details.requiredSigs, nil
 	}
 
+	// Check for null data script.
+	if isNullDataScript(scriptVersion, pkScript) {
+		// Null data transactions have no addresses or required signatures.
+		return NullDataTy, nil, 0, nil
+	}
+
 	// Fall back to slow path.  Ultimately these are intended to be replaced by
 	// faster variants based on the unparsed raw scripts.
 
@@ -818,18 +825,6 @@ func ExtractPkScriptAddrs(pkScript []byte, chainParams *chaincfg.Params) (Script
 			addrs = append(addrs, addr)
 		}
 
-	case ScriptHashTy:
-		// A pay-to-script-hash script is of the form:
-		//  OP_HASH160 <scripthash> OP_EQUAL
-		// Therefore the script hash is the 2nd item on the stack.
-		// Skip the script hash if it's invalid for some reason.
-		requiredSigs = 1
-		addr, err := btcutil.NewAddressScriptHashFromHash(pops[1].data,
-			chainParams)
-		if err == nil {
-			addrs = append(addrs, addr)
-		}
-
 	case WitnessV0ScriptHashTy:
 		// A pay-to-witness-script-hash script is of the form:
 		//  OP_0 <32-byte hash>
@@ -842,15 +837,13 @@ func ExtractPkScriptAddrs(pkScript []byte, chainParams *chaincfg.Params) (Script
 			addrs = append(addrs, addr)
 		}
 
-	case NullDataTy:
-		// Null data transactions have no addresses or required
-		// signatures.
-
 	case NonStandardTy:
 		// Don't attempt to extract addresses or required signatures for
 		// nonstandard transactions.
 	}
 
+	// Don't attempt to extract addresses or required signatures for nonstandard
+	// transactions.
 	return scriptClass, addrs, requiredSigs, nil
 }
 